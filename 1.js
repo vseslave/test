6979dbfe4eef41066e905e33c2f7a442bafcfb36
Vasiliy <vseslave@gmail.com>
2019-06-05 16:22:58 +0300

    начальная стадия. первый коммит

---
+++ 1.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let a=5;
+console.log(a);
+
+let persone = {
+    name: "Vasyliy",
+    age: 25,
+    isMarried: false
+};
+console.log(persone.age);
+
+//let arr = ['1', '2', '3'];
+//console.log(arr[1]);
+//alert("Heloo");
+//let ans = confirm("ты тут?");
+//console.log(ans);
+//let ans = +prompt("are you there?", "No");
+//console.log(typeof(ans));
+//console.log(arr[2]+"dfdfsdf");
+
+let incr=10,
+    decr=10;
+
+console.log(incr++);
+console.log(decr--);
+console.log(incr);
+console.log(5/2);
+console.log("2"===2);
+
+let isC = true,
+    icCLO = true;
+console.log(!isC || !icCLO);
+
